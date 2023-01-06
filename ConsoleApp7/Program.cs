1c26b56bdacd0856f03f86aeccc43064f6f0627c
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-06 19:50:27 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO; /*IO=input output*/
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("c:\\");
+            var dirList = di.GetDirectories();
+            foreach (var folder in dirList)
+            {
+                Console.WriteLine(folder.Name);
+                Dön(folder);
+            }
+        }
+        static void Dön(DirectoryInfo di)
+        {
+            try
+            {
+                var dirList = di.GetDirectories();
+                foreach (var folder in dirList)
+                {
+                    Console.WriteLine(folder.Name);
+                    Dön(folder);
+                }
+            }
+            catch (Exception)
+            {
+              
+            }
+            
+        }
+    }
+}
